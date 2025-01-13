9b243a659d9861f0359c3d941e253fcf4889db5b
banditofsmoke <skeletonenglish@gmail.com>
2025-01-13 21:40:57 +0200

    change2sign in

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -12,6 +12,7 @@ export default function SignInPage() {
     <div className="container max-w-screen-xl mx-auto py-12">
       <div>
         <h2>Environment Variables</h2>
+        <p>seb is a great guy</p>
         <ul>
           {Object.entries(envVars).map(([key, value]) => (
             <li key={key}>
